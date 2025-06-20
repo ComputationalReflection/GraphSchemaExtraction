b58fae45f4bcee716001440f1488e70574ff49aa
mastema98 <maniega98@hotmail.com>
2025-06-20 18:03:55 +0200

    Web english, icons and footer

---
+++ Web/.angular/cache/17.3.1/vite/deps/@angular_animations.js
@@ -0,0 +1,49 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-JU6ZUHZ2.js";
+import "./chunk-BKR2UZTI.js";
+import "./chunk-CQER6OOX.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
